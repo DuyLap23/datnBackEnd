353ff95bc59119d4488a29ff3290040a69aa85e4
DuyLap <hoangduylap124@gmail.com>
2024-09-06 20:44:43 +0700

    category

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -16,13 +16,19 @@ class CategoryController extends Controller
     const PATH_UPLOAD = 'categories';
     public function index()
     {
-        $categories = Category::query()->with(['children'])->where('parent_id', null)->get();
+        $categories = Category::query()
+            ->with(['children'])
+            ->where('parent_id', null)
+            ->get();
         $categoryParent = Category::query()->where('parent_id', null)->get();
-        return response()->json([
-            'success' => true,
-            'message' => 'Category retrieved successfully',
-            'data' => ['categories' => $categories, 'categoryParen' => $categoryParent]
-        ],200);
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Lấy thành công danh mục',
+                'data' => ['categories' => $categories, 'categoryParent' => $categoryParent],
+            ],
+            200,
+        );
     }
 
     /**
@@ -30,26 +36,27 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        
-        try {
-       
-            $data = $request->validate([
-                'name' => ['require', 'max:255'],
-                'image' => ['require', 'mime:jpeg,jpg,png,svg,webp', 'max:1500'],
-                'parent_id'=> ['nullable', 'exists:categories,id'],
-            ]);
-            DB::beginTransaction();
+        DB::beginTransaction();
 
-             if ($request->hasFile('image')) {
-                $data['image'] = Storage::put(self::PATH_UPLOAD, $request->file('image'));
+        try {
+            $data = $request->validate([
+                'name' => ['required', 'max:255'],
+                'image' => ['required', 'mimes:jpeg,jpg,png,svg,webp', 'max:1500'],
+                'parent_id' => ['nullable','exists:categories,id'],
+            ]);
+
+            if ($request->hasFile('image')) {
+                $data['image'] = $request->file('image')->store(self::PATH_UPLOAD, 'public');
             }
-        
+
             Category::query()->create($data);
+
             DB::commit();
+
             return response()->json(
                 [
                     'success' => true,
-                    'message' => 'Category created successfully.',
+                    'message' => 'Thêm danh mục thành công.',
                 ],
                 201,
             );
@@ -58,9 +65,10 @@ class CategoryController extends Controller
             return response()->json(
                 [
                     'success' => false,
-                    'message' => $exception->getMessage(),
-
-                ],500
+                    'message' => 'Thêm danh mục thất bại',
+                    'error' => $exception->getMessage()
+                ],
+                500,
             );
         }
     }
@@ -70,7 +78,26 @@ class CategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $category = Category::findOrFail($id);
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Lấy thành công dữ liệu của bản ghi ' . $id,
+                    'data' => $category,
+                ],
+                200,
+            );
+        } catch (\Exception $exception) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'lấy dữ liệu không thành công',
+                    'error' => $exception->getMessage()
+                ],
+                500,
+            );
+        }
     }
 
     /**
@@ -78,7 +105,52 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $data = $request->validate([
+                'name' => ['required', 'max:255'],
+                'image' => ['mimes:jpeg,jpg,png,svg,webp', 'max:1500'],
+                'parent_id' => ['nullable','exists:categories,id'],
+            ]);
+
+            $model = Category::query()->findOrFail($id);
+
+            if ($request->hasFile('image')) {
+                $data['image'] = $request->file('image')->store(self::PATH_UPLOAD, 'public');
+
+                $image_old = $model->image;
+            } else {
+                $image_old = null;
+            }
+
+            $model->update($data);
+
+            if ($image_old && Storage::exists($image_old)) {
+                Storage::delete($image_old);
+            }
+
+            DB::commit();
+
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Cập nhật danh mục thành công.',
+                    'data' => $model,
+                ],
+                201,
+            );
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Cập nhật danh mục thất bại',
+                    'error' => $exception->getMessage(),
+                ],
+                500,
+            );
+        }
     }
 
     /**
@@ -86,6 +158,34 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            
+            $model = Category::findOrFail($id);
+
+            $model->delete();
+            
+            if ($model->image && Storage::exists($model->image)) {
+                Storage::delete($model->image);
+            }
+
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Xoá danh mục thành công.',
+                ],
+                200,
+            );
+        
+        } catch (\Exception $e) {
+           
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Xoá danh mục không thành công.',
+                    'error' => $e->getMessage()
+                ],
+                500,
+            );
+        }
     }
 }
---
+++ lang/vi/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Thông tin đăng nhập không khớp.',
+    'password' => 'Mật khẩu cung cấp không chính xác.',
+    'throttle' => 'Quá nhiều lần đăng nhập thất bại. Vui lòng thử lại sau :seconds giây.',
+
+];
