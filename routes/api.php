416fbfce5129bcb58ea5bcc2a9ea2a77460ffe74
thuanvm04 <vuminhthuan004@gmail.com>
2024-09-06 22:54:02 +0700

    Action brand

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\API\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\BrandController;
+use App\Http\Controllers\API\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('brands', BrandController::class);
 
