eea869689050f4a4ac6b3dc1e642bfb844155e68
cuongdv <cuongdv@infoplusvn.com>
2024-09-20 01:28:55 +0700

    User_Authorization_10_9

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\Http\Controllers\API\AddressController;
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\RegisterController;
 use App\Http\Controllers\API\Auth\ResetPassword;
 use App\Http\Controllers\API\Auth\UserController;
+use App\Http\Controllers\API\BannerMktController;
 use App\Http\Controllers\API\TagController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\BrandController;
+use App\Http\Controllers\API\CartController;
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\CommentController;
+use App\Http\Controllers\API\FavouriteListController;
+use App\Http\Controllers\API\OrderController;
+use App\Http\Controllers\API\OrderItemController;
+use App\Http\Controllers\API\ProductColorController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\ProductImageController;
+use App\Http\Controllers\API\ProductSizeController;
+use App\Http\Controllers\API\ProductVariantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,14 +55,49 @@ Route::group(
 );
 Route::group(
     [
-        'middleware' => 'api',
+        'middleware' => ['api', 'role:admin'],
         'prefix' => 'admin',
     ],
     function ($router) {
         Route::apiResource('categories', CategoryController::class);
         Route::apiResource('brands', BrandController::class);
         Route::apiResource('tags', TagController::class);
+        Route::apiResource('addresses', AddressController::class);
+        Route::apiResource('banners', BannerMktController::class);
+        Route::apiResource('products', ProductController::class);
+        Route::apiResource('product/colors', ProductColorController::class);
+        Route::apiResource('product/images', ProductImageController::class);
+        Route::apiResource('product/sizes', ProductSizeController::class);
+        Route::apiResource('product/variants', ProductVariantController::class);
         Route::get('users', [UserController::class, 'index']);
         Route::get('users/{id}', [UserController::class, 'show']);
-    },
+    }
+);
+Route::group(
+    [
+        'middleware' => ['api', 'role:staff'],
+        'prefix' => 'staff',
+    ],
+    function ($router) {
+        Route::apiResource('orders', OrderController::class);
+        Route::apiResource('order/items', OrderItemController::class);
+        Route::apiResource('favourites', FavouriteListController::class);
+        Route::apiResource('comments', CommentController::class);
+        Route::apiResource('carts', CartController::class);
+    }
+);
+
+Route::group(
+    [
+        'middleware' => ['api', 'role:customer'],
+        'prefix' => 'customer',
+    ],
+    function ($router) {
+        Route::get('products', [ProductController::class, 'index']);
+        Route::get('products/{id}', [ProductController::class, 'show']);
+        Route::post('carts', [CartController::class, 'store']);
+        Route::get('carts', [CartController::class, 'index']);
+        Route::post('orders', [OrderController::class, 'store']);
+        Route::get('orders/{id}', [OrderController::class, 'show']);
+    }
 );
