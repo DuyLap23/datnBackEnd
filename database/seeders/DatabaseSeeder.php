facbc867fefac9ee1e0b9856a9128a461f313ad3
cuongdv <damcuongict@gmail.com>
2024-10-19 23:39:43 +0700

    add collum and data  sample

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             CategorySeeder::class,
-            UserSeeder::class,  VoucherSeeder::class, BrandSeeder::class,ProductSeeder::class
+            UserSeeder::class,  VoucherSeeder::class, BrandSeeder::class,ProductSeeder::class, OrdersTableSeeder::class,
+            OrderItemsTableSeeder::class,
         ]);
     }
 }
