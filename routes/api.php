87084c6fcd7a6ee35c97d5c6af5768a83f063ca6
cuongdv <damcuongict@gmail.com>
2024-10-20 00:52:23 +0700

    add detall

--- routes/api.php
+++ routes/api.php
@@ -132,7 +132,7 @@ Route::group(
         Route::delete('banners/{id}', [BannerMktController::class, 'destroy']);
 
         Route::get('/orders', [OrderManagementController::class, 'index']);
-        Route::get('/orders/{id}', [OrderManagementController::class, 'show']);
+        Route::get('/orders/{id}', [OrderManagementController::class, 'detall']);
         Route::put('/orders/{id}/status', [OrderManagementController::class, 'updateStatus']);
         Route::put('/orders/{id}', [OrderManagementController::class, 'update']);
         Route::post('/orders/{id}/refund', [OrderManagementController::class, 'refund']);
